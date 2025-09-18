dcce2a7f77f1f67b2eb3cc35c96f62d8ed51ad00
Aphile2556627 <2556627@students.wits.ac.za>
2025-09-18 11:10:44 +0200

    camera changes

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function init() {
     // Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(5, 3, 5);
-    camera.lookAt(0, 0, 0);
+    camera.lookAt(0.5, 0.5, 0.5);
 
     // Renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
